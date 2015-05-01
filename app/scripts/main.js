4bf35ff8fe3a67d4017147d00860bfc476d47a83
aluborsky <andrew.luborsky@gmail.com>
2015-04-30 20:00:56 -0400

    Smooth scroll working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,32 @@
 
 (function($) {
   $(document).ready(function() {
-    // $('.img-holder').imageScroll();
+
+  	//parallax effect
+    //$('.img-holder').imageScroll();
+    
+    //scrollspy
     $('body').scrollspy({
     	target: '.navbar'
     });
 
+    //smooth scroll
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 900);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+	//tool tips
     $(function () {
     	$('[data-toggle="tooltip"]').tooltip();
     });

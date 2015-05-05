0208161eb85298f83dbe09ced32765b32d59166b
aluborsky <andrew.luborsky@gmail.com>
2015-05-05 00:23:37 -0400

    Modal width change

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function (grunt) {
           },
           pages: {
             options: {
-              remote: '!!! PASTE YOUR GITHUB REPO URL HERE',
+              remote: 'https://github.com/aluborsky/fed-final-project.git',
               branch: 'gh-pages'
             }
           },
